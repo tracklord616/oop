14755cd22da1143f44c915709053d1608570c10e
tracklord616 <44816423+tracklord616@users.noreply.github.com>
2019-02-21 20:41:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a,b,z,t;
+    cout << "vvedite a, b" << endl;
+    cin >> a >> b;
+    if (a<b) z = sqrt(abs(a*a-b*b));
+    else z = 1 - cos(a)*sin(b);
+    if (z < b) t = pow((z+a*a*b), 1/3);
+    if (z == b) t = 1 - log10(z) + cos(a*a*b);
+    if ((z>b) || (a !=0) || (z !=0)) t = 1/cos(z*a);
+    cout << "t = " << t << " z = " << z << endl;
+    return 0;
+}
