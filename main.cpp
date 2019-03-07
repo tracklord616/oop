5607de298e916bd4680b0d01027b9a9dee6b9f21
tracklord616 <44816423+tracklord616@users.noreply.github.com>
2019-03-07 17:46:38 +0300

    pra2
    praktika 2

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-    double a,b,z,t;
+double a,b,z,t;
     cout << "vvedite a, b" << endl;
     cin >> a >> b;
     if (a<b) z = sqrt(abs(a*a-b*b));
-    else z = 1 - cos(a)*sin(b);
-    if (z < b) t = pow((z+a*a*b), 1/3);
+        else z = 1 - cos(a)*sin(b);
+    if (z < b) t = pow((z+a*a*b), 1.0/3.0);
     if (z == b) t = 1 - log10(z) + cos(a*a*b);
-    if ((z>b) || (a !=0) || (z !=0)) t = 1/cos(z*a);
+    if ((z>b) && (a !=0) && (z !=0)) t = 1/cos(z*a);
     cout << "t = " << t << " z = " << z << endl;
     return 0;
 }
