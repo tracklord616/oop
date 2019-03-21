806f66ca7df3f18af03e82028cff7586a434b87b
tracklord616 <44816423+tracklord616@users.noreply.github.com>
2019-03-21 17:10:06 +0300

    Add files via upload
    praktika 5

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 using namespace std;
-
-void set_array(int** array, int count)
+void sorty(int a[],int n)
 {
-    srand(time(0));
-    for (int i=0; i<count; i++)
-        for (int j=0; j<count; j++) array[i][j] = 10 + rand() % 40;
+    for (int i=0; i<n;i++)
+        for (int j=i; j<n;j++) if (a[i]<a[j]) swap(a[i],a[j]);
 }
-void get_array(int** array, int count)
+void sorty(double a[],int n)
 {
-    for (int i=0; i< count; i++) {
-        for (int j = 0; j < count; j++) cout << array[i][j] << ' ';
-        cout << "\n";
-    }
+    for (int i=0; i<n;i++)
+        for (int j=i; j<n;j++) if (a[i]<a[j]) swap(a[i],a[j]);
 }
 int main() {
-    int size;
-    cout << "ukazhite razmer" << endl;
-    cin >> size;
-    int  **couple = new int*[size];
-    for (int i = 0; i < size; i++) couple[i] = new int[size];
-    set_array(couple, size);
-    get_array(couple,size);
-    return 0;
+int n;
+cin >> n;
+int a[n];
+double b[n];
+srand(time( 0 ) );
+for (int i=0; i < n; i++) a[i] = rand() % 101;
+for (int i=0; i < n; i++) b[i] = (rand() % 101) * 1.57;
+for (int i=0; i < n; i++) cout << a[i] << ' ';
+cout << endl;
+for (int i=0; i < n; i++) cout << b[i] << ' ';
+cout << endl;
+cout << endl;
+    sorty(a,n);
+    sorty(b,n);
+for (int i=0; i < n; i++) cout << a[i] << ' ';
+cout << endl;
+for (int i=0; i < n; i++) cout << b[i] << ' ';
+return 0;
 }
