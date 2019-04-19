51901efaf68b44fbfafd4de43fba0fc3a31a22e2
tracklord616 <44816423+tracklord616@users.noreply.github.com>
2019-04-19 19:48:09 +0300

    Add files via upload
    pra5

--- main.cpp
+++ main.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
-#include <ctime>
+#include <utility>
 using namespace std;
-void sorty(int a[],int n)
-{
-    for (int i=0; i<n;i++)
-        for (int j=i; j<n;j++) if (a[i]<a[j]) swap(a[i],a[j]);
+int* sortl(int a[], int n){
+    for (int i = 0; i <= n; i++)
+        for (int j = i; j <= n; j++)
+            if (a[i]<a[j]) swap(a[i],a[j]);
+    return a;
 }
-void sorty(double a[],int n)
-{
-    for (int i=0; i<n;i++)
-        for (int j=i; j<n;j++) if (a[i]<a[j]) swap(a[i],a[j]);
+
+double* sortl(double a[], int n){
+    for (int i = 0; i <= n; i++)
+        for (int j = i; j <= n; j++)
+            if (a[i]<a[j]) swap(a[i],a[j]);
+    return a;
 }
+
 int main() {
-int n;
-cin >> n;
-int a[n];
-double b[n];
-srand(time( 0 ) );
-for (int i=0; i < n; i++) a[i] = rand() % 101;
-for (int i=0; i < n; i++) b[i] = (rand() % 101) * 1.57;
-for (int i=0; i < n; i++) cout << a[i] << ' ';
+int q[] = {12,5,57,21,17,39};
+double k[]= {12.6, 57.21, 17.39,18.54,5.2};
+int *t = q;
+sortl(t,6);
+for(int i = 0; i < 6; i++) cout << q[i] << ' ';
 cout << endl;
-for (int i=0; i < n; i++) cout << b[i] << ' ';
-cout << endl;
-cout << endl;
-    sorty(a,n);
-    sorty(b,n);
-for (int i=0; i < n; i++) cout << a[i] << ' ';
-cout << endl;
-for (int i=0; i < n; i++) cout << b[i] << ' ';
-return 0;
+double *o = k;
+sortl(o,5);
+for(int i = 0; i < 5; i++) cout << k[i] << ' ';
+ return 0;
 }
