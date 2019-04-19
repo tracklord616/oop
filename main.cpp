b115b0c558e9f5e4e7a6137e74a2a5458f733c98
tracklord616 <44816423+tracklord616@users.noreply.github.com>
2019-04-19 20:32:28 +0300

    Add files via upload
    praktika 6

--- main.cpp
+++ main.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
-#include <utility>
 using namespace std;
-int* sortl(int a[], int n){
-    for (int i = 0; i <= n; i++)
-        for (int j = i; j <= n; j++)
-            if (a[i]<a[j]) swap(a[i],a[j]);
-    return a;
-}
-
-double* sortl(double a[], int n){
-    for (int i = 0; i <= n; i++)
-        for (int j = i; j <= n; j++)
-            if (a[i]<a[j]) swap(a[i],a[j]);
-    return a;
-}
-
+class property {
+protected:
+   int worth;
+public:
+    property(int worth){
+        this->worth = worth;
+    }
+    virtual double nalog() = 0;
+};
+class car: public property{
+public:
+    double nalog(){
+        return worth/200;
+    }
+    car(int worth): property(worth){
+    nalog();
+    }
+};
+class appartment: public property{
+public:
+    double nalog(){
+        return worth/1000;
+    }
+    appartment(int worth):property(worth){
+    nalog();
+    }
+};
+class countryHouse: public property{
+public:
+    double nalog(){
+        return worth/500;
+    }
+    countryHouse(int worth):property(worth){
+        nalog();
+    }
+};
 int main() {
-int q[] = {12,5,57,21,17,39};
-double k[]= {12.6, 57.21, 17.39,18.54,5.2};
-int *t = q;
-sortl(t,6);
-for(int i = 0; i < 6; i++) cout << q[i] << ' ';
-cout << endl;
-double *o = k;
-sortl(o,5);
-for(int i = 0; i < 5; i++) cout << k[i] << ' ';
- return 0;
+property *a[7];
+double sum;
+appartment q = appartment(125000),w = appartment(123456), e = appartment(987654);
+car r = car(111111), t = car(222222);
+countryHouse y = countryHouse(333333), u = countryHouse(444444);
+
+a[0] = &q;
+a[1] = &w;
+a[2] = &e;
+a[3] = &r;
+a[4] = &t;
+a[5] = &y;
+a[6] = &u;
+
+for (int i=0; i<=6; i++){
+    sum += a[i]->nalog();
+}
+cout << sum;
+
+    return 0;
 }
