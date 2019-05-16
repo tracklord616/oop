60de8aee17ee47f544dcdb005bb535e4d721bc1b
tracklord616 <44816423+tracklord616@users.noreply.github.com>
2019-05-16 14:46:02 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
-#include <fstream>
+#include <string>
 using namespace std;
+class animal{
+protected:
+    int height;
+    int mass;
+public:
+    virtual void sound() =0;
+    animal(int height, int mass){
+        this->height = height;
+        this->mass = mass;
+    }
+};
+class cat:public animal{
+public:
+    void sound(){
+        cout << "meow " << "height is " << height << ", " << "mass is " << mass << endl;
+    }
+    cat(int height, int mass): animal(height, mass){}
+};
+class dog:public animal{
+public:
+    void sound(){
+        cout << "gav " << "height is " << height << ", "   << "mass is " << mass << endl;
+    }
+    dog(int height, int mass): animal(height, mass){}
+};
+class parrot:public animal{
+protected:
+    string n;
+public:
+    void sound(){
+        cout << n << " " << "height is " << height << ", "   << "mass is " << mass << endl;
+    }
+    parrot(int height, int mass, string n): animal(height, mass){
+        this->n = n;
+    }
+};
+
 int main() {
-    string kek;
-    ofstream fut("sos.txt");
-    cout << "enter your line" << endl;
-    cin >> kek;
-    fut << kek;
-    fut.close();
+    dog a = dog(12,5);
+    cat b = cat(6,5);
+    string p = "lalka";
+    parrot c = parrot(3,4,p);
+    a.sound();
+    b.sound();
+    c.sound();
     return 0;
 }
